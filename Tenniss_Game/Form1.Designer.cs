b73540dd6ab7fe243b4e755cd344881735efd285
CoffeeBon <blue3197@gmail.com>
2017-11-08 18:12:13 +0900

    주석 추가

--- Tenniss_Game/Form1.Designer.cs
+++ Tenniss_Game/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.RightBar = new System.Windows.Forms.Button();
             this.Ball = new System.Windows.Forms.RadioButton();
             this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TopBar
@@ -46,7 +47,7 @@
             this.TopBar.TabIndex = 0;
             this.TopBar.Text = "T";
             this.TopBar.UseVisualStyleBackColor = true;
-            this.TopBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopButton_MouseMove);
+            this.TopBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseMove);
             // 
             // BottomBar
             // 
@@ -57,7 +58,7 @@
             this.BottomBar.TabIndex = 1;
             this.BottomBar.Text = "B";
             this.BottomBar.UseVisualStyleBackColor = true;
-            this.BottomBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopButton_MouseMove);
+            this.BottomBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseMove);
             // 
             // LeftBar
             // 
@@ -68,7 +69,7 @@
             this.LeftBar.TabIndex = 2;
             this.LeftBar.Text = "L";
             this.LeftBar.UseVisualStyleBackColor = true;
-            this.LeftBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftButton_MouseMove);
+            this.LeftBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftBar_MouseMove);
             // 
             // RightBar
             // 
@@ -79,7 +80,7 @@
             this.RightBar.TabIndex = 3;
             this.RightBar.Text = "R";
             this.RightBar.UseVisualStyleBackColor = true;
-            this.RightBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftButton_MouseMove);
+            this.RightBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftBar_MouseMove);
             // 
             // Ball
             // 
@@ -96,21 +97,38 @@
             // Timer1
             // 
             this.Timer1.Enabled = true;
-            this.Timer1.Interval = 33;
+            this.Timer1.Interval = 3;
             this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(473, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1009, 573);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Ball);
             this.Controls.Add(this.RightBar);
             this.Controls.Add(this.LeftBar);
             this.Controls.Add(this.BottomBar);
             this.Controls.Add(this.TopBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
@@ -126,6 +144,7 @@
         private System.Windows.Forms.Button RightBar;
         private System.Windows.Forms.RadioButton Ball;
         private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Tenniss_Game/Form1.cs
+++ Tenniss_Game/Form1.cs
@@ -17,6 +17,9 @@ namespace Tenniss_Game
             InitializeComponent();
         }
 
+        /// <summary>
+        ///  Form1의 Mouse Move
+        /// </summary>
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             //마우스의 무브 이벤트에 대한 좌표 찾기
@@ -33,122 +36,155 @@ namespace Tenniss_Game
             RightBar.Top = LeftBar.Top;
         }
 
-        private void TopButton_MouseMove(object sender, MouseEventArgs e)
+        /// <summary>
+        /// Top & Bottom Bar의 Mouse Move
+        /// </summary>
+        private void TopBar_MouseMove(object sender, MouseEventArgs e)
         {
             TopBar.Left = TopBar.Left + e.X;
             BottomBar.Left = TopBar.Left;
         }
 
-        //private void BotButton_MouseMove(object sender, MouseEventArgs e)
-        //{
-        //    TopButton.Left = TopButton.Left + e.X;
-        //    BotButton.Left = TopButton.Left;
-        //}
-
-        private void LeftButton_MouseMove(object sender, MouseEventArgs e)
+        /// <summary>
+        /// Left & Right Bar의 Mouse Move 
+        /// </summary>
+        private void LeftBar_MouseMove(object sender, MouseEventArgs e)
         {
             LeftBar.Top = LeftBar.Top + e.Y;
             RightBar.Top = LeftBar.Top;
         }
 
-        //private void RightButton_MouseMove(object sender, MouseEventArgs e)
-        //{
-        //    LeftButton.Top = LeftButton.Top + e.Y;
-        //    RightButton.Top = LeftButton.Top;
-        //}
-
+        /// <summary>
+        /// Ball의 움직임 
+        /// </summary>
         private void Timer1_Tick(object sender, EventArgs e)
         {
+            //
+            // 폼에 Ball의 좌표 표시
+            //
             this.Text = $"Ball의 좌표({(this.Ball.Left).ToString()}, {(this.Ball.Top).ToString()})";
-
+            //
+            // Ball의 스피드 증가
+            int nSpeed = 2;
+            //
+            // 공이 날아가는 방향
+            #region Location of Ball in Form1
+            //
+            //1시 방향
             if (this.Ball.Text == "1")
             {
-                this.Ball.Left += 1;
-                this.Ball.Top -= 2;
+                this.Ball.Left += 1 * nSpeed;
+                this.Ball.Top -= 2 * nSpeed;
             }
+            //
+            //2시 방향
             else if (this.Ball.Text == "2")
             {
-                this.Ball.Left += 2;
-                this.Ball.Top -= 1;
+                this.Ball.Left += 2 * nSpeed;
+                this.Ball.Top -= 1 * nSpeed;
             }
+            //
+            //3시 방향
             else if (this.Ball.Text == "3")
             {
-                this.Ball.Left += 3;
-                this.Ball.Top -= 0;
+                this.Ball.Left += 3 * nSpeed;
+                this.Ball.Top -= 0 * nSpeed;
             }
+            //
+            //4시 방향
             else if (this.Ball.Text == "4")
             {
-                this.Ball.Left += 2;
-                this.Ball.Top += 1;
+                this.Ball.Left += 2 * nSpeed;
+                this.Ball.Top += 1 * nSpeed;
             }
+            //
+            //5시 방향
             else if (this.Ball.Text == "5")
             {
-                this.Ball.Left += 1;
-                this.Ball.Top += 2;
+                this.Ball.Left += 1 * nSpeed;
+                this.Ball.Top += 2 * nSpeed;
             }
+            //
+            //6시 방향
             else if (this.Ball.Text == "6")
             {
-                this.Ball.Left += 0;
-                this.Ball.Top += 3;
+                this.Ball.Left += 0 * nSpeed;
+                this.Ball.Top += 3 * nSpeed;
             }
+            //
+            //7시 방향
             else if (this.Ball.Text == "7")
             {
-                this.Ball.Left -= 1;
-                this.Ball.Top += 2;
+                this.Ball.Left -= 1 * nSpeed;
+                this.Ball.Top += 2 * nSpeed;
             }
+            //
+            //8시 방향
             else if (this.Ball.Text == "8")
             {
-                this.Ball.Left -= 2;
-                this.Ball.Top += 1;
+                this.Ball.Left -= 2 * nSpeed;
+                this.Ball.Top += 1 * nSpeed;
             }
+            //
+            //9시 방향
             else if (this.Ball.Text == "9")
             {
-                this.Ball.Left -= 3;
-                this.Ball.Top += 0;
+                this.Ball.Left -= 3 * nSpeed;
+                this.Ball.Top += 0 * nSpeed;
             }
+            //
+            //10시 방향
             else if (this.Ball.Text == "10")
             {
-                this.Ball.Left -= 2;
-                this.Ball.Top -= 1;
+                this.Ball.Left -= 2 * nSpeed;
+                this.Ball.Top -= 1 * nSpeed;
             }
+            //
+            //11시 방향
             else if (this.Ball.Text == "11")
             {
-                this.Ball.Left -= 1;
-                this.Ball.Top -= 2;
+                this.Ball.Left -= 1 * nSpeed;
+                this.Ball.Top -= 2 * nSpeed;
             }
+            //
+            //12시 방향
             else if (this.Ball.Text == "12")
             {
-                this.Ball.Left -= 0;
-                this.Ball.Top -= 3;
+                this.Ball.Left -= 0 * nSpeed;
+                this.Ball.Top -= 3 * nSpeed;
             }
-
-            if ((this.Ball.Left < 0) || 
-                (this.Ball.Right > this.ClientRectangle.Width) || 
-                (this.Ball.Top < 0) || 
-                (this.Ball.Bottom > this.ClientRectangle.Height))
+            #endregion
+            //
+            // 공이 폼 밖으로 나갔을 때 Game Over
+            //
+            #region Out of form1 of Ball
+            if ((this.Ball.Left < 0) ||
+                    (this.Ball.Right > this.ClientRectangle.Width) ||
+                    (this.Ball.Top < 0) ||
+                    (this.Ball.Bottom > this.ClientRectangle.Height))
             {
                 this.Timer1.Enabled = false;
+                System.Media.SystemSounds.Asterisk.Play();
                 MessageBox.Show("Game Over");
             }
-
+            #endregion
+            //
+            //Top Bar에 부딪혔을때 공이 1인 상태면 5로 변환(튕김)
+            //
             if (this.TopBar.Bounds.Contains(this.Ball.Left, this.Ball.Top) && (Ball.Text == "1"))
-            { //Top Bar에 부딪혔을때 공이 1인 상태면 5로 변환(튕김)
+            { 
                 Ball.Text = "5";
             }
-
-            //TopButton  테두리 안에 라디오 버튼 테두리가 있나?
-            if ((this.BottomBar.Bounds.Contains(this.Ball.Left, this.Ball.Top) == true) ||
-                (this.LeftBar.Bounds.Contains(this.Ball.Left, this.Ball.Top) == true) ||
-                (this.RightBar.Bounds.Contains(this.Ball.Left, this.Ball.Top) == true))
-            {
-                this.Timer1.Enabled = false;
-                MessageBox.Show("충돌감지");
-            }
         }
-
+        //
+        // 폼에 마우스를 클릭했을 때.
+        //
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Right) //오른쪽 클릭 하면 시계방향
+            //
+            //오른쪽 클릭 하면 시계방향, 왼쪽 클릭하면 반시계 방향
+            //
+            if (e.Button == MouseButtons.Right) 
             {
                 this.Ball.Text = (Convert.ToInt16(this.Ball.Text) + 1).ToString();
 
@@ -157,7 +193,7 @@ namespace Tenniss_Game
                     this.Ball.Text = "1";
                 }
             }
-            else  //왼쪽 클릭 하면 반시계방향
+            else  
             {
                 this.Ball.Text = (Convert.ToInt16(this.Ball.Text) - 1).ToString();
                 if (this.Ball.Text == "0")
